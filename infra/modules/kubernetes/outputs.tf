9d689f8f6b93c34955ef130d8ad378b74dadab6e
Vitalii Nedvyga <vnedvyga@Vitaliis-MBP.fritz.box>
2025-11-28 19:29:43 +0100

    specify region in second endpoint group of ga

--- infra/modules/kubernetes/outputs.tf
+++ infra/modules/kubernetes/outputs.tf
@@ -2,3 +2,8 @@ output "apigw_lb" {
   value = data.aws_lb.apigw_lb.arn
   description = "AWS LB (NLB) arn created by contour api gateway"
 }
+
+output "aws_region" {
+  value = var.aws_region
+  description = "AWS region of provisioned LB (NLB)"
+}
